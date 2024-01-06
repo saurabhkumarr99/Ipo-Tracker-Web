ef4e5f9b181812001c8f217e39b5b3187d3969b5
saurabhkumarr99 <saurabhkumarr99@gmail.com>
2024-01-07 00:29:03 +0530

    Updated Register page

--- src/allComponents/Register.js
+++ src/allComponents/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Form, Button, FormGroup, FormLabel, FormControl, FormCheck } from 'react-bootstrap';
+import { Form, Button, FormGroup, FormLabel, FormControl, FormCheck, Col, Container, Row } from 'react-bootstrap';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -36,82 +36,88 @@ const Register = () => {
       <div style={{ backgroundColor: '#1890ff', padding: '10px 0' }}>
         <h1 style={{ color: 'white', textAlign: 'center' }}>Register yourself</h1>
       </div>
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '30px' }}>
-        <div style={{ width: '30%', padding: '20px', border: '1px solid #ccc', borderRadius: '5px' }}>
-          <div style={{ textAlign: 'center', marginBottom: '20px' }}>
-            <img
-              src="/images/RegImg.png"
-              alt="Registration Image"
-              style={{ width: '100%', height: 'auto', borderTopLeftRadius: '5px', borderTopRightRadius: '5px' }}
-            />
-          </div>
-          <Form onSubmit={handleSubmit}>
-            <FormGroup controlId="username">
-              <FormLabel>Username</FormLabel>
-              <FormControl
-                type="text"
-                placeholder="Enter username"
-                required
-                value={formData.username}
-                onChange={handleInputChange}
-              />
-            </FormGroup>
-
-            <FormGroup controlId="email">
-              <FormLabel>Email</FormLabel>
-              <FormControl
-                type="email"
-                placeholder="Enter email"
-                required
-                value={formData.email}
-                onChange={handleInputChange}
-              />
-            </FormGroup>
-
-            <FormGroup controlId="password">
-              <FormLabel>Password</FormLabel>
-              <FormControl
-                type="password"
-                placeholder="Password"
-                required
-                value={formData.password}
-                onChange={handleInputChange}
-              />
-            </FormGroup>
-
-            <FormGroup controlId="confirmPassword">
-              <FormLabel>Confirm Password</FormLabel>
-              <FormControl
-                type="password"
-                placeholder="Confirm password"
-                required
-                value={formData.confirmPassword}
-                onChange={handleInputChange}
-              />
-            </FormGroup>
-
-            <FormGroup controlId="agreement">
-              <FormCheck>
-                <FormCheck.Input
-                  type="checkbox"
-                  required
-                  checked={formData.agreement}
-                  onChange={handleInputChange}
+      <Container style={{ marginTop: '30px' }}>
+        <Row className="justify-content-center">
+          <Col md={6} order-md={2}>
+            <div style={{ padding: '20px', border: '1px solid #ccc', borderRadius: '5px' }}>
+              <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+                <img
+                  src="/images/RegImg.png"
+                  alt="Registration Image"
+                  style={{ width: '100%', height: 'auto', borderTopLeftRadius: '5px', borderTopRightRadius: '5px' }}
                 />
-                <FormCheck.Label>
-                  I have read the <a href="/terms">agreement</a>
-                </FormCheck.Label>
-              </FormCheck>
-            </FormGroup>
+              </div>
 
-            <div style={{ textAlign: 'center' }}>
-              <Button type="submit" variant="primary">
-                Register
-              </Button>
+              <Form onSubmit={handleSubmit}>
+                <FormGroup controlId="username">
+                  <FormLabel>Username</FormLabel>
+                  <FormControl
+                    type="text"
+                    placeholder="Enter username"
+                    required
+                    value={formData.username}
+                    onChange={handleInputChange}
+                  />
+                </FormGroup>
+
+                <FormGroup controlId="email">
+                  <FormLabel>Email</FormLabel>
+                  <FormControl
+                    type="email"
+                    placeholder="Enter email"
+                    required
+                    value={formData.email}
+                    onChange={handleInputChange}
+                  />
+                </FormGroup>
+
+                <FormGroup controlId="password">
+                  <FormLabel>Password</FormLabel>
+                  <FormControl
+                    type="password"
+                    placeholder="Password"
+                    required
+                    value={formData.password}
+                    onChange={handleInputChange}
+                  />
+                </FormGroup>
+
+                <FormGroup controlId="confirmPassword">
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl
+                    type="password"
+                    placeholder="Confirm password"
+                    required
+                    value={formData.confirmPassword}
+                    onChange={handleInputChange}
+                  />
+                </FormGroup>
+
+                <FormGroup controlId="agreement">
+                  <FormCheck>
+                    <FormCheck.Input
+                      type="checkbox"
+                      required
+                      checked={formData.agreement}
+                      onChange={handleInputChange}
+                    />
+                    <FormCheck.Label>
+                      I have read the <a href="/terms">agreement</a>
+                    </FormCheck.Label>
+                  </FormCheck>
+                </FormGroup>
+
+                <div style={{ textAlign: 'center' }}>
+                  <Button type="submit" variant="primary">
+                    Register
+                  </Button>
+                </div>
+              </Form>
             </div>
-          </Form>
-        </div>
-      </div>
+          </Col>
+
+        </Row>
+      </Container>
       <ToastContainer />
     </div>
   );
